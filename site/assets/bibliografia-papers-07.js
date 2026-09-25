b41ff855e3e0462b51de185509474cdd45c4613c
anavvanzin <ana@anavanzin.com>
2026-09-25 03:53:15 -0300

    feat(biblio): papers, parte 7/7

---
+++ site/assets/bibliografia-papers-07.js
@@ -0,0 +1,196 @@
+window.BIBLIO_DATA.papers.push(...[
+{
+ "title": "A representação visual da justiça nos periódicos anarquistas (1901-1927)",
+ "authors": [
+  "A GAWRYSZESKI"
+ ],
+ "year": 2009,
+ "venue": "A GAWRYSZESKI - Anais do II Encontro Nacional de Estudos da Imagem …, 2009 - uel.br",
+ "citation_count": 2,
+ "abstract": "… A Justiça é, em geral, representada, reconhecida na figura de uma mulher em vestes leves, com olhos vendados, … Ela é Thêmis, a deusa da Justiça. É claro que não nos propomos a …",
+ "url": "https://www.uel.br/eventos/eneimagem/anais/trabalhos/pdf/Gawryzweski_Alberto.pdf",
+ "themes": [
+  "Justiça de olhos vendados"
+ ],
+ "source_query": "Justiça de olhos vendados"
+},
+{
+ "title": "De olhos vendados: a invisibilidade da união homoafetiva no Congresso Nacional ea oficialização pelo Supremo Tribunal Federal",
+ "authors": [
+  "JFM Marin"
+ ],
+ "venue": "JFM Marin - GÊNERO E DIVERSIDADE SEXUAL - academia.edu",
+ "citation_count": 0,
+ "abstract": "… com seus olhos vendados, … Justiça possui seus olhos vendados para garantir que ninguém se faça superior ao outro, a maior parte do Congresso Nacional brasileiro venda seus olhos …",
+ "url": "https://www.academia.edu/download/62092743/GENERO_E_DIVERSIDADE_SEXUAL_percursos_e20200213-14348-bfcutk.pdf#page=71",
+ "themes": [
+  "Justiça de olhos vendados"
+ ],
+ "source_query": "Justiça de olhos vendados"
+},
+{
+ "title": "A justiça ea finalidade do Direito",
+ "authors": [
+  "PC Sanvito"
+ ],
+ "year": 2012,
+ "venue": "PC Sanvito - Jus Humanum: Revista Eletrônica de …, 2012 - revistapos.cruzeirodosul.edu.br",
+ "citation_count": 2,
+ "abstract": "… A Iustitia é a deusa romana da justiça, sendo representada com os olhos vendados e segurando somente a balança nas mãos, pois, para os romanos, os direitos valiam mais que a …",
+ "url": "https://revistapos.cruzeirodosul.edu.br/jus_humanum/article/view/78",
+ "themes": [
+  "Justiça de olhos vendados"
+ ],
+ "source_query": "Justiça de olhos vendados"
+},
+{
+ "title": "O SEGREDO DOS OLHOS (VENDADOS) DA JUSTIÇA",
+ "authors": [
+  "A de Medeiros Venturini",
+  "SF Machado"
+ ],
+ "venue": "A de Medeiros Venturini, SF Machado",
+ "citation_count": 0,
+ "themes": [
+  "Justiça de olhos vendados"
+ ],
+ "source_query": "Justiça de olhos vendados"
+},
+{
+ "title": "Legal emblems and the art of law",
+ "authors": [
+  "P Goodrich"
+ ],
+ "year": 1945,
+ "venue": "P Goodrich - American history, 1945 - cambridge.org",
+ "citation_count": 222,
+ "abstract": "The emblem book was invented by the humanist lawyer Andrea Alciato in 1531. The … emblem books is critical to their purpose and interest. This book outlines the history of the emblem …",
+ "url": "https://www.cambridge.org/uz/universitypress/subjects/law/legal-history/legal-emblems-and-art-law-iobiter-depictai-vision-governance",
+ "themes": [
+  "Peter Goodrich legal emblems"
+ ],
+ "source_query": "Goodrich legal emblems"
+},
+{
+ "title": "Devising law: on the philosophy of legal emblems",
+ "authors": [
+  "P Goodrich"
+ ],
+ "year": 2013,
+ "venue": "P Goodrich - Law, Culture and Visual Studies, 2013 - Springer",
+ "citation_count": 19,
+ "abstract": "… Military, religious, administrative and legal images found juridical codi fi cation and … century of the legal emblem book. This chapter traces the complex lineage of the emblem book and …",
+ "url": "https://link.springer.com/chapter/10.1007/978-90-481-9322-6_1",
+ "themes": [
+  "Peter Goodrich legal emblems"
+ ],
+ "source_query": "Goodrich legal emblems"
+},
+{
+ "title": "Specters of Law: Why the History of the Legal Spectable Has Not Been Written",
+ "authors": [
+  "P Goodrich"
+ ],
+ "year": 2011,
+ "venue": "P Goodrich - UC Irvine L. Rev., 2011 - HeinOnline",
+ "citation_count": 38,
+ "abstract": "… It would be best if the image could be reproducedthe art of the emblem in the legal text has been too long defunct, but assuming that space is limited I will use another and even more …",
+ "url": "https://access.heinonline.com/hol-cgi-bin/get_pdf.cgi?handle=hein.journals/ucirvlre1&section=36",
+ "themes": [
+  "Peter Goodrich legal emblems"
+ ],
+ "source_query": "Goodrich legal emblems"
+},
+{
+ "title": "The evidence of things not seen",
+ "authors": [
+  "P Goodrich"
+ ],
+ "year": 2015,
+ "venue": "P Goodrich - Genealogies of Legal Vision, 2015 - books.google.com",
+ "citation_count": 3,
+ "abstract": "… , however, in the early emblem tradition, show arms dominating law rather than the lyre, with a … For the purposes of interpreting emblems of justice and law, of eyes and blindfolds, the …",
+ "url": "https://books.google.com/books?hl=zh-CN&lr=&id=yVnLCQAAQBAJ&oi=fnd&pg=PA53&dq=Goodrich+legal+emblems&ots=8P4wgMsWKt&sig=iNU9dDKszeATHr_h0RTJfhVx4bk",
+ "themes": [
+  "Peter Goodrich legal emblems"
+ ],
+ "source_query": "Goodrich legal emblems"
+},
+{
+ "title": "A Gesture: A Review of Peter Goodrich, Legal Emblems and the Art of             Law: Obiter Depicta as the Vision of Governance (Cambridge University Press …",
+ "authors": [
+  "P Haldar"
+ ],
+ "year": 2014,
+ "venue": "P Haldar - 2014 - Taylor & Francis",
+ "citation_count": 2,
+ "abstract": "… While he is not alone in observing the connection between law and emblems, Goodrich might be the first to analyse the legal provenance and consequences of the jurisdiction of …",
+ "url": "https://www.tandfonline.com/doi/pdf/10.5235/17521483.8.2.304",
+ "themes": [
+  "Peter Goodrich legal emblems"
+ ],
+ "source_query": "Goodrich legal emblems"
+},
+{
+ "title": "Legal Emblems and the Return of the Impressed",
+ "authors": [
+  "C Landauer"
+ ],
+ "year": 2015,
+ "venue": "C Landauer - Yale JL & Human., 2015 - HeinOnline",
+ "citation_count": 2,
+ "abstract": "… and Missing Limbs: Goodrich on Emblems,\" will work through Goodrich's iconography of … Legal Emblems and the Art of Law, we are introduced to a new and arcane world with Goodrich …",
+ "url": "https://access.heinonline.com/hol-cgi-bin/get_pdf.cgi?handle=hein.journals/yallh27&section=13",
+ "themes": [
+  "Peter Goodrich legal emblems"
+ ],
+ "source_query": "Goodrich legal emblems"
+},
+{
+ "title": "Iconomus: Review of: Peter Goodrich, Legal Emblems and the Art of Law",
+ "authors": [
+  "C Behrmann"
+ ],
+ "year": 2014,
+ "venue": "C Behrmann - Rechtsgeschichte–Legal History, 2014 - rg.lhlt.mpg.de",
+ "citation_count": 0,
+ "abstract": "… legal emblems, but about the overall significance of a critical apprehension of the visible for the law… part of the judicial process, Peter Goodrich surpasses wellknown discussions about …",
+ "url": "https://rg.lhlt.mpg.de/index.php/rg/article/download/876/894",
+ "themes": [
+  "Peter Goodrich legal emblems"
+ ],
+ "source_query": "Goodrich legal emblems"
+},
+{
+ "title": "Visiocracy: On the futures of the fingerpost",
+ "authors": [
+  "P Goodrich"
+ ],
+ "year": 2013,
+ "venue": "P Goodrich - Critical Inquiry, 2013 - journals.uchicago.edu",
+ "citation_count": 31,
+ "abstract": "… law sources that established the reign of legal emblems and the modes of visual governance that became the visiocratic regime that we myopic modern lawyers inherit along with the …",
+ "url": "https://www.journals.uchicago.edu/doi/full/10.1086/670043",
+ "themes": [
+  "Peter Goodrich legal emblems"
+ ],
+ "source_query": "Goodrich legal emblems"
+},
+{
+ "title": "The art of law",
+ "authors": [
+  "S Huygebaert",
+  "G Martyn",
+  "V Paumen…"
+ ],
+ "year": 2016,
+ "venue": "S Huygebaert, G Martyn, V Paumen… - Three centuries of justice …, 2016 - Springer",
+ "citation_count": 24,
+ "abstract": "… -) Representations of Justice’focuses on post-1795 legal iconography, which perfectly fits in … of historical legal iconography and iconology, particularly focusing on the subject of justice …",
+ "url": "https://link.springer.com/content/pdf/10.1007/978-3-319-90787-1.pdf",
+ "themes": [
+  "recent production 2015-2026"
+ ],
+ "source_query": "law art justice iconography (2015-2026, sort=date)"
+}
+]);
